88257fdea11776a85df23a7e601c8e381d876549
Chiranjeevi Makam <137588934+chiranjeevi-makam@users.noreply.github.com>
2024-08-28 15:44:09 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+const App=()=>{ 
+  const[speed, setSpeed]=useState(0) 
+  const increase=()=>{
+    if(speed>200){
+      setSpeed(200)
+    }else{
+      setSpeed((prev)=>(prev+10))
+    }
+  } 
+  const Decrease=()=>{
+    if(speed>0){
+      setSpeed((prev)=>(prev-10))
+    }else{
+      setSpeed(0)
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return(
+    <div className="backgroundsetting"> 
+    <h1 className="heading">SPEEDOMETER</h1> 
+    <img src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png" alt="speedometer" className="photo"/> 
+    <h3 className="speedCount">speed is {speed}mph</h3> 
+    <p className="instructions">Minmum Limit 0mph; Maximium Limit 200mph</p> 
+    <div>
+      <button type="button" className="accelerateButton" onClick={increase}>Accelarate</button> 
+      <button type="button" className="applyBrakes" onClick={Decrease}>Apply Brakes</button>
     </div>
-  );
+
+    </div>
+  )
 }
 
 export default App;
